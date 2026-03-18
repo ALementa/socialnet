f612be9d320eed5697e29d1c081f54a43cba7588
ALementa <lementalex@gmail.com>
2026-03-18 11:52:10 +0000

    Fix middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 
-export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!_next|.*\\..*).*)"],
+// };
+export { default, config } from "./src/proxy";
