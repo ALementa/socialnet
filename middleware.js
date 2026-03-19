a9a92436b9d00e86e3eacdd310407be11b212ade
ALementa <lementalex@gmail.com>
2026-03-19 07:51:16 +0000

    chenge proxy and middleware

--- middleware.js
+++ middleware.js
@@ -8,16 +8,35 @@
 
 // export { default, config } from "./src/proxy";
 
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isProtectedRoute = createRouteMatcher(["/users(.*)", "/posts(.*)"]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isProtectedRoute(req)) {
+//     await auth.protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+// };
+
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/users(.*)", "/posts(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/users(.*)", "/books(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) {
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) {
     await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
